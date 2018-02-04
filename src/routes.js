80c7aa82f423abc63c8d8cb657b7582603af4fd6
Reese Woodard <reese695@gmail.com>
2018-02-04 07:44:52 -0800

    Compressed images

--- src/routes.js
+++ src/routes.js
@@ -3,7 +3,6 @@ import {browserHistory, Route, IndexRoute, Router} from 'react-router';
 import App from './App';
 import Map from './MainMap';
 import Error404 from './Error404';
-import Map from './Map';
 import About from './About';
 
 const routes = (
